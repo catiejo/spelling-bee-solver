b905cd0bb2a91eda41a74d6ff2440626c480299c
Catie Jo <catiejo@caties-mbp.zhaw.ch>
2018-11-15 15:04:52 +0100

    code simplification so it only goes through the dictionary once

--- README.md
+++ README.md
@@ -10,8 +10,8 @@ A simple python script to solve the NY Times spelling bee puzzle. (www.nytimes.c
 ## How to use the script
 The `solve()` function takes a string of any length, treating lowercase letters as normal letters, and uppercase letters as mandatory (the yellow middle letter in the NY Times puzzle).
 * `spelling_bee.solve("inkblOt")`
-* `spelling_bee.solve("inkblOt", dictionary_path="../path/to/your/dictionary")` *(default: dictionary.json included in repo)*
-* `spelling_bee.solve("inkblOt", minimum_word_length=2)` *(default: 4)*
+* `spelling_bee.solve("inkblOt", dict_path="../path/to/your/dictionary")` *(default: dictionary.json included in repo)*
+* `spelling_bee.solve("inkblOt", min_letters=2)` *(default: 4)*
 
 (you can also assign the solution to a local variable: `solution = spelling_bee.solve("inkblOt")`)
 
@@ -19,9 +19,8 @@ The `solve()` function takes a string of any length, treating lowercase letters 
 I'm using a scrabble json dictionary I found [here](https://github.com/benjamincrom/scrabble/blob/master/scrabble/dictionary.json), but you can easily sub in your own dictionary. However, I'm expecting a file format of `["word1", "word2", ..., "wordN"]`, so modify the `json_to_list()` function if you want to use something else.
 
 On the subject of dictionaries, I have no clue what dictionary the actual puzzle is using, so there are usually a number of words that my solver finds that aren't actually solutions to the NY Times puzzle. I'm a nerd so I was curious what those words were... and also if there are words my dictionary is missing. I cross check my list against the actual solution as follows:
-1. Click on "Yesterday's Answers" and copy all of the words into a new json file 
-2. JSON-ify them by putting the whole list in brackets, adding quotes around each word, and a comma after all words except the last (see gratiFy_solution.json). 
+1. Click on "Yesterday's Answers" and copy all of the words into a new json file
+2. JSON-ify them by putting the whole list in brackets, adding quotes around each word, and a comma after all words except the last (see gratiFy_solution.json).
 3. You can see the words that I found that weren't part of the NY Times solution by typing `spelling_bee.compare_solutions(solution, "your_solution_file.json)`.
 
 Any words that weren't in the solution, and also any words that the solver didn't find, are printed to the console.
-
--- spelling_bee.py
+++ spelling_bee.py
@@ -1,13 +1,5 @@
 import json
 
-# returns a string of the unique, alphabetized letters in the given word
-def get_letters(word):
-    letters = ""
-    for char in word:
-        if char not in letters:
-            letters += char
-    return ''.join(sorted(letters))
-
 # json files must be in form ["word1", "word2", "word3"] (see dictionary.json)
 def json_to_list(filepath):
     with open(filepath) as f:
@@ -23,44 +15,22 @@ def compare_solutions(generated_solution, path_to_actual_solution):
     print ("Words that are in the solution that the solver didn't find:")
     print list(actual_solution.difference(generated_solution))
 
-# Generates a dict where the key is a string of the unique, alphabetized letters
-# in the word, and the key is the list of all words using that letterself. For
-# example, an entry could be {"bo": ["boob", "booboo"]}
-def process_dictionary(path_to_dictionary, min_word_length):
-    dictionary = json_to_list(path_to_dictionary)
-    dict_by_letters = {}
+# this goes through each word in the dictionary and checks if it belongs in the solution
+def get_words_in_solution(spelling_bee, dictionary, min_letters):
+    bee_letters = set([letter.lower() for letter in list(spelling_bee)])
+    required_letters = set([letter.lower() for letter in list(spelling_bee) if letter.isupper()])
+    solution = []
     for word in dictionary:
-        if len(word) < min_word_length:
+        if len(word) < min_letters:
             continue
-        letters_for_word = get_letters(word)
-        if letters_for_word not in dict_by_letters:
-            dict_by_letters[letters_for_word] = []
-        dict_by_letters[letters_for_word].append(word)
-    return dict_by_letters
+        letters = set([list(word)])
+        if letters.issubset(bee_letters) and required_letters.issubset(letters):
+            solution.append(word)
+    return solution
 
-# Goes through the dictionary and removes entries that either (1) contain letters
-# that aren't in the spelling bee puzzle or (2) don't contain the mandatory letters.
-def prune_dictionary(dictionary_dict, spelling_bee):
-    spelling_bee_letters = set([letter.lower() for letter in list(spelling_bee)])
-    mandatory_letters = set([letter.lower() for letter in list(spelling_bee) if letter.isupper()])
-    for key in dictionary_dict.keys():
-        key_letters = set(list(key))
-        if not key_letters.issubset(spelling_bee_letters) or not mandatory_letters.issubset(key_letters):
-            del dictionary_dict[key]
-
-# e.g. solve("inkOblt"), solve("inkOblt", dictionary_path="dictionary.json")
+# e.g. solve("inkOblt"), solve("inkOblt", dict_path="dictionary.json")
 def solve(spelling_bee, **optional_parameters):
-    dictionary_path = 'dictionary.json'
-    min_word_length = 4
-    if 'dictionary_path' in optional_parameters:
-        dictionary_path = optional_parameters['dictionary_path']
-    if 'min_word_length' in optional_parameters:
-        min_word_length = optional_parameters['min_word_length']
-    dictionary_dict = process_dictionary(dictionary_path, min_word_length)
-    prune_dictionary(dictionary_dict, spelling_bee)
-    words_in_solution = []
-    # dictionary_dict.values() is a list of lists of words.
-    for words in list(dictionary_dict.values()):
-        for word in words:
-            words_in_solution.append(word)
-    return words_in_solution
+    dictionary_path = optional_parameters['dict_path'] if ('dict_path' in optional_parameters) else 'dictionary.json'
+    min_word_length = optional_parameters['min_letters'] if ('min_letters' in optional_parameters) else 4
+    dictionary = json_to_list(dictionary_path)
+    return get_words_in_solution(spelling_bee, dictionary, min_word_length)
